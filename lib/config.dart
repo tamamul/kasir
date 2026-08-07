41bd2874cb61413cc94d633bd0b88d281833f82b
tamamul <36046825+tamamul@users.noreply.github.com>
2026-08-07 22:42:01 +0700

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -2,5 +2,5 @@ class AppConfig {
   // Ganti dengan URL Web App Apps Script kamu — sama persis dengan
   // yang dipakai di config.php pada versi web
   static const String appsScriptUrl =
-      'https://script.google.com/macros/s/GANTI_DENGAN_DEPLOYMENT_ID/exec';
+      'https://script.google.com/macros/s/AKfycbx0EnUXkpqZIXhUrIJ5wvOvIBuZx-VVegdrBVEFEn9Y6BPV3LCl21PUFEe3N3WAnOsO/exec';
 }
