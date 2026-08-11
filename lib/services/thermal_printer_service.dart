801536fefa40fcf16720d302e5e69ad25dd54559
tamamul <36046825+tamamul@users.noreply.github.com>
2026-08-11 22:22:04 +0700

    Update thermal_printer_service.dart

--- lib/services/thermal_printer_service.dart
+++ lib/services/thermal_printer_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_thermal_printer/flutter_thermal_printer.dart';
+import 'package:flutter_thermal_printer/utils/printer.dart';
 
 class ThermalPrinterService {
   static final FlutterThermalPrinter _plugin =
